017edcec6dbd51eb36a9dbe593fbc2a1d97317cb
Hakyur <drogotovsky@gmail.com>
2025-04-30 19:17:45 +0300

    feature(GUARD-118): added api gateway

---
+++ application/backend/api-gateway/src/main/java/ru/rogotovskiy/apigateway/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package ru.rogotovskiy.apigateway.jwt;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import ru.rogotovskiy.apigateway.service.JwtService;
+
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        if (isSecured(request)) {
+
+            String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                return onError(exchange, "Missing or invalid Authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String token = authHeader.substring(7);
+
+            try {
+                if (!jwtService.isTokenValid(token)) {
+                    return onError(exchange, "Invalid token", HttpStatus.UNAUTHORIZED);
+                }
+
+                String userId = jwtService.getUserId(token);
+                ServerHttpRequest modifiedRequest = request.mutate()
+                        .header("X-User-Id", userId)
+                        .build();
+
+                return chain.filter(exchange.mutate().request(modifiedRequest).build());
+            } catch (Exception e) {
+                return onError(exchange, "Token validation failed", HttpStatus.UNAUTHORIZED);
+            }
+        }
+        return chain.filter(exchange);
+    }
+
+
+    private Mono<Void> onError(ServerWebExchange exchange, String error, HttpStatus status) {
+        exchange.getResponse().setStatusCode(status);
+        return exchange.getResponse().setComplete();
+    }
+
+    private boolean isSecured(ServerHttpRequest request) {
+        String path = request.getURI().getPath();
+        return path.startsWith("/api/reviews") ||
+                path.startsWith("/api/user");
+    }
+}
