06fb0c10ec1c66dbc1c45b6b9f101e692f058aea
Hakyur <drogotovsky@gmail.com>
2025-06-02 13:53:12 +0300

    refactor: change RouteType enum class

--- application/backend/map/src/main/java/ru/rogotovskiy/map/enums/RouteType.java
+++ application/backend/map/src/main/java/ru/rogotovskiy/map/enums/RouteType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum RouteType {
     AUTO("auto", "Автомобильный", "car"),
-    PEDESTRIAN("pedestrian", "Пешеходный", "foot"),
+    PEDESTRIAN("pedestrian", "Пеший", "foot"),
     BICYCLE("bicycle", "Велосипедный", "bike");
 
     private final String tourType;
